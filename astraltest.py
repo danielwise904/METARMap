bee4c2739637c7d4885b065a643fa7fff5a41397
Danielwise <danielwise904@gmail.com>
2020-10-15 11:21:12 -0500

    Added comments

--- astraltest.py
+++ astraltest.py
@@ -1,20 +1,25 @@
+# astral README: https://astral.readthedocs.io/en/latest/index.html
+
 import datetime
 import astral
 from astral import LocationInfo
 from astral.geocoder import database, lookup
 from astral.sun import sun
 
-CITY = "Austin"
+# Configuation
+CITY = "Austin"							# Choose from list of cities in README
 LED_DAY_BRIGHTNESS	= 0.2				# Float from 0.0 (min) to 1.0 (max)
 LED_NIGHT_BRIGHTNESS= 0.08				# Float from 0.0 (min) to 1.0 (max)
 
-now_utc = datetime.datetime.utcnow()
+# Setup
+now_utc = datetime.datetime.utcnow()	# astral times are in zulu
 now_local = datetime.datetime.now()
 year = now_local.date().year
 month = now_local.date().month
 day = now_local.date().day
 city = lookup(CITY,database())
-s = sun(city.observer, date=datetime.date(year, month, day))
+s = sun(city.observer, date=datetime.date(year, month, day))	# query with local day to keep sunset and sunrise on local day not zulu day
+#times in zulu
 sunrise = (s["sunrise"]).replace(tzinfo=None)
 sunset = (s["sunset"]).replace(tzinfo=None)
 dawn = (s["dawn"]).replace(tzinfo=None)
